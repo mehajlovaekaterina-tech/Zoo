05c5eb9b021927278616a8f1291638ca0f4bb687
mehaj <mehaj@DESKTOP-1JNMMHB>
2026-02-24 18:22:53 +0300

    создала файл Bird.cs с кодом из задания

---
+++ Zoo/Bird.cs
@@ -0,0 +1,8 @@
+﻿public class Bird : Animal
+{
+    public Bird(string name, int age) : base(name, age) { }
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} чирикает");
+    }
+}
