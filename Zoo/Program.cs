fdff5e946689d782f3db2fb0b78586a8dc24c8ce
Настя <kashirinanatya201101@gmail.com>
2026-02-24 18:18:50 +0300

    создала файл Mammal.cs с кодом из задания

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System.Xml.Linq;
+public class Mammal : Animal
+{
+    public Mammal(string name, int age) : base(name, age) { }
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} рычит");
+    }
+}
