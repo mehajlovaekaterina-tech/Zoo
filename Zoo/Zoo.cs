df3ce47a84fdd890a97f0ad9c0387e32f985b3c5
Настя <kashirinanatya201101@gmail.com>
2026-02-24 18:28:17 +0300

    создала файл Zoo.cs с кодом

---
+++ Zoo/Zoo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Zoo
+{
+    private List<Animal> _animals = new List<Animal>();
+    public void AddAnimal(Animal animal)
+    {
+        _animals.Add(animal);
+    }
+    public void ShowAll()
+    {
+        foreach (var animal in _animals)
+        {
+            animal.MakeSound();
+        }
+    }
+}
