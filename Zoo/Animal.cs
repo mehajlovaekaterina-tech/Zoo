c6f25b6a0701987c57e051f3bef2b93f79f2867a
mehaj <mehaj@DESKTOP-1JNMMHB>
2026-02-24 18:14:46 +0300

    создала файл Animal.cs с кодом из задания

---
+++ Zoo/Animal.cs
@@ -0,0 +1,16 @@
+﻿public class Animal
+{
+    private string _name;
+    private int _age;
+    public string Name => _name;
+    public int Age => _age;
+    public Animal(string name, int age)
+    {
+        _name = name;
+        _age = age;
+    }
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Животное издает звук");
+    }
+}
