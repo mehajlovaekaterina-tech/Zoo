3ad92819cc493435a727752bc35963a019e0f6d2
mehaj <mehaj@DESKTOP-1JNMMHB>
2026-02-24 19:31:52 +0300

    Создание ветки и дополнение кода в Animal.cs

--- Zoo/Animal.cs
+++ Zoo/Animal.cs
@@ -1,16 +1,32 @@
-﻿public class Animal
+﻿using System.Threading.Channels;
+
+public class Animal
 {
     private string _name;
     private int _age;
+    private int _soundCount;
+
     public string Name => _name;
     public int Age => _age;
+
     public Animal(string name, int age)
     {
         _name = name;
         _age = age;
+        _soundCount = 1; 
     }
+
+    public int GetSoundCount()
+    {
+        return _soundCount;
+    }
+
     public virtual void MakeSound()
     {
+        _soundCount++; 
         Console.WriteLine("Животное издает звук");
     }
-}
+    
+} 
+
+
