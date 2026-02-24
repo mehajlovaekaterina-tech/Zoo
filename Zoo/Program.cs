a728a9c45072754ece936c6842cda8c94acc5bb7
mehaj <mehaj@DESKTOP-1JNMMHB>
2026-02-24 18:31:36 +0300

    добавила в Program.cs код из задания

---
+++ Zoo/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Zoo zoo = new Zoo();
+        zoo.AddAnimal(new Mammal("Лев", 5));
+        zoo.AddAnimal(new Bird("Орел", 3));
+        zoo.ShowAll();
+    }
+}
