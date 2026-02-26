0d1b04f08934997df507ea867bb3bcc305ae4682
Настя <kashirinanatya201101@gmail.com>
2026-02-26 15:14:49 +0300

    "добро пожаловать в зоопарк"

--- Zoo/Program.cs
+++ Zoo/Program.cs
@@ -2,6 +2,7 @@
 {
     static void Main()
     {
+        Console.WriteLine("Добро пожаловать в зоопарк");
         Zoo zoo = new Zoo();
         zoo.AddAnimal(new Mammal("Лев", 5, "молоко"));
         zoo.AddAnimal(new Bird("Орел", 3, "семечко"));
